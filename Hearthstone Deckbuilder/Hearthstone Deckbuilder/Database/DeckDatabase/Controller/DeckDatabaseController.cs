65f236989c2e21e099d9a78036602a48fa5482ba
luca_hausheer <luca_hausheer>
2016-05-02 15:58:44 +0200

    Created Folder Structure

---
+++ Hearthstone Deckbuilder/Hearthstone Deckbuilder/Database/DeckDatabase/Controller/DeckDatabaseController.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hearthstone_Deckbuilder.Database.DeckDatabase.Controller
+{
+    class DeckDatabaseController
+    {
+
+    }
+}
